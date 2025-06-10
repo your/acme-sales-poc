f2cf9a437b63f97b7dda9bf70fc56afd2f04c170
Giuseppe Lobraico <your@users.noreply.github.com>
2025-06-10 14:54:08 +0200

    Add unit testing setup with rspec

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -82,21 +82,21 @@ class Basket
   end
 end
 
-delivery_rules = [
-  { min: 0, max: 50, charge: 4.95 },
-  { min: 50, max: 90, charge: 2.95 },
-  { min: 90, max: Float::INFINITY, charge: 0 }
-]
+# delivery_rules = [
+#   { min: 0, max: 50, charge: 4.95 },
+#   { min: 50, max: 90, charge: 2.95 },
+#   { min: 90, max: Float::INFINITY, charge: 0 }
+# ]
 
-catalogue = [
-  Product.new("R01", "Red Widget", 32.95),
-  Product.new("G01", "Green Widget", 24.95),
-  Product.new("B01", "Blue Widget", 7.95)
-]
+# catalogue = [
+#   Product.new("R01", "Red Widget", 32.95),
+#   Product.new("G01", "Green Widget", 24.95),
+#   Product.new("B01", "Blue Widget", 7.95)
+# ]
 
-red_widget_offer = BuyOneGetOneHalfPriceOffer.new("R01")
-delivery_calculator = DeliveryChargeCalculator.new(delivery_rules)
-basket = Basket.new(catalogue, delivery_calculator, [red_widget_offer])
+# red_widget_offer = BuyOneGetOneHalfPriceOffer.new("R01")
+# delivery_calculator = DeliveryChargeCalculator.new(delivery_rules)
+# basket = Basket.new(catalogue, delivery_calculator, [red_widget_offer])
 
 # basket.add("R01")
 # basket.add("R01")
